38051577a99d5eb3a1eb8ddd4a45e963c49a479c
Roman <spriggan1992@gmail.com>
2020-11-03 00:20:17 +0800

    .

--- lib/screens/collage_screen/collage_screen.dart
+++ lib/screens/collage_screen/collage_screen.dart
@@ -6,6 +6,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:multi_image_picker/multi_image_picker.dart';
 
+List<StaggeredTile> staggeredTiles = <StaggeredTile>[
+  StaggeredTile.count(2, 2),
+  StaggeredTile.count(2, 1),
+  StaggeredTile.count(1, 2),
+  StaggeredTile.count(1, 1),
+  StaggeredTile.count(2, 2),
+  StaggeredTile.count(1, 2),
+  StaggeredTile.count(1, 1),
+  StaggeredTile.count(3, 1),
+  StaggeredTile.count(1, 1),
+  StaggeredTile.count(4, 1),
+]..shuffle();
+
+// List<StaggeredTile> shuffledStaggeredTiles = staggeredTiles..shuffle();
+// List<Widget> buildRow() {
+//     int height = 100;
+//     Random random = new Random();
+//     int randomNum = random.nextInt(3) + 1;
+
+//   }
 class CollageScreen extends StatelessWidget {
   static String id = 'collage_screen';
   const CollageScreen({Key key}) : super(key: key);
@@ -19,55 +39,314 @@ class CollageScreen extends StatelessWidget {
           body: BackgroundWrapper(
             child: BlocBuilder<CollageBloc, CollageState>(
               builder: (context, state) {
-                return Container(
-                  constraints: BoxConstraints(
-                    maxHeight: SizeConfig.blockSizeVertical * 100,
-                  ),
-                  child: StaggeredGridView.countBuilder(
-                    crossAxisCount: 4,
-                    itemCount: 2,
-                    // itemCount: state.images.length,
-                    itemBuilder: (context, index) =>
-                        // Container(child: Text('text $index')),
-                        AssetThumb(
-                            quality: 100,
-                            asset: state.images[index],
-                            width: defaultSize.toInt() * 5,
-                            height: defaultSize.toInt() * 5),
-                    // width: defaultSize.toInt() * 60,
-                    // height: defaultSize.toInt() * 60),
-                    staggeredTileBuilder: (index) =>
-                        StaggeredTile.count(2, index.isEven ? 5 : 5),
-                    mainAxisSpacing: 4.0,
-                    crossAxisSpacing: 4.0,
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Stack(
+                    children: [
+                      Expanded(
+                        child: ResizableWidget(
+                          child: AssetThumb(
+                              quality: 100,
+                              asset: state.images[0],
+                              width: defaultSize.toInt() * 20,
+                              height: defaultSize.toInt() * 20),
+                        ),
+                      ),
+                    ],
                   ),
                 );
-
-                // return Column(
-                //   children: [
-                //     Center(child: TitleHolder(title: 'Choose View')),
-                //     Expanded(
-                //       child: StaggeredGridView.countBuilder(
-                //         crossAxisCount: 4,
-                //         itemCount: state.images.length,
-                //         itemBuilder: (context, index) =>
-                //             // Container(child: Text('text $index')),
-                //             AssetThumb(
-                //                 quality: 100,
-                //                 asset: state.images[index],
-                //                 width: defaultSize.toInt() * 50,
-                //                 height: defaultSize.toInt() * 50),
-                //         staggeredTileBuilder: (index) =>
-                //             StaggeredTile.count(1, index.isEven ? 1 : 3),
-                //         mainAxisSpacing: 4.0,
-                //         crossAxisSpacing: 4.0,
-                //       ),
-                //     ),
-                //   ],
-                // );
               },
             ),
           )),
+    );
+  }
+}
+
+class YourLayoutDelegate extends MultiChildLayoutDelegate {
+  YourLayoutDelegate({this.position});
+  final double position;
+  @override
+  void performLayout(Size size) {
+    Size leadingSize = Size.zero;
+    if (hasChild(1)) {
+      leadingSize = layoutChild(
+        1,
+        BoxConstraints.loose(size),
+      );
+    }
+    if (hasChild(2)) {
+      final secondSize = layoutChild(
+        2,
+        BoxConstraints(
+          maxWidth: size.width,
+          maxHeight: size.height,
+        ),
+      );
+
+      positionChild(
+        2,
+        Offset(
+          leadingSize.width, // This will place child 2 to the right of child 1.
+          size.height / 7 -
+              secondSize.height / 2, // Centers the second child vertically.
+        ),
+      );
+    }
+  }
+
+  @override
+  bool shouldRelayout(YourLayoutDelegate oldDelegate) {
+    return oldDelegate.position != position;
+  }
+}
+
+class ResizableWidget extends StatefulWidget {
+  const ResizableWidget({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+  @override
+  _ResizableWidgetState createState() => _ResizableWidgetState();
+}
+
+const ballDiameter = 20.0;
+
+class _ResizableWidgetState extends State<ResizableWidget> {
+  double height = 400;
+  double width = 200;
+
+  double top = 0;
+  double left = 0;
+
+  void onDrag(double dx, double dy) {
+    var newHeight = height + dy;
+    var newWidth = width + dx;
+
+    setState(() {
+      height = newHeight > 0 ? newHeight : 0;
+      width = newWidth > 0 ? newWidth : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultSize = SizeConfig.defaultSize;
+    return Stack(
+      // fit: StackFit.loose,
+      // clipBehavior: Clip.hardEdge,
+      // overflow: Overflow.clip,
+      children: [
+        Positioned(
+          top: top,
+          left: left,
+          child: Container(
+              height: height,
+              width: width,
+              color: Colors.red[100],
+              child: widget.child),
+        ),
+        // top left
+        Positioned(
+          top: top - ballDiameter / 2,
+          left: left - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var mid = (dx + dy) / 2;
+              var newHeight = height - 2 * mid;
+              var newWidth = width - 2 * mid;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+                width = newWidth > 0 ? newWidth : 0;
+                top = top + mid;
+                left = left + mid;
+              });
+            },
+          ),
+        ),
+        // top middle
+        Positioned(
+          top: top - ballDiameter / 2,
+          left: left + width / 2 - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var newHeight = height - dy;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+                top = top + dy;
+              });
+            },
+          ),
+        ),
+        // top right
+        Positioned(
+          top: top - ballDiameter / 2,
+          left: left + width - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var mid = (dx + (dy * -1)) / 2;
+
+              var newHeight = height + 2 * mid;
+              var newWidth = width + 2 * mid;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+                width = newWidth > 0 ? newWidth : 0;
+                top = top - mid;
+                left = left - mid;
+              });
+            },
+          ),
+        ),
+        // center right
+        Positioned(
+          top: top + height / 2 - ballDiameter / 2,
+          left: left + width - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var newWidth = width + dx;
+
+              setState(() {
+                width = newWidth > 0 ? newWidth : 0;
+              });
+            },
+          ),
+        ),
+        // bottom right
+        Positioned(
+          top: top + height - ballDiameter / 2,
+          left: left + width - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var mid = (dx + dy) / 2;
+
+              var newHeight = height + 2 * mid;
+              var newWidth = width + 2 * mid;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+                width = newWidth > 0 ? newWidth : 0;
+                top = top - mid;
+                left = left - mid;
+              });
+            },
+          ),
+        ),
+        // bottom center
+        Positioned(
+          top: top + height - ballDiameter / 2,
+          left: left + width / 2 - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var newHeight = height + dy;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+              });
+            },
+          ),
+        ),
+        // bottom left
+        Positioned(
+          top: top + height - ballDiameter / 2,
+          left: left - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var mid = ((dx * -1) + dy) / 2;
+
+              var newHeight = height + 2 * mid;
+              var newWidth = width + 2 * mid;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+                width = newWidth > 0 ? newWidth : 0;
+                top = top - mid;
+                left = left - mid;
+              });
+            },
+          ),
+        ),
+        //left center
+        Positioned(
+          top: top + height / 2 - ballDiameter / 2,
+          left: left - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var newWidth = width - dx;
+
+              setState(() {
+                width = newWidth > 0 ? newWidth : 0;
+                left = left + dx;
+              });
+            },
+          ),
+        ),
+        // center center
+        Positioned(
+          top: top + height / 2 - ballDiameter / 2,
+          left: left + width / 2 - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              setState(() {
+                top = top + dy;
+                left = left + dx;
+                print(dy);
+                if (top > SizeConfig.blockSizeVertical * 100 + height / 2)
+                  top = 0;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ManipulatingBall extends StatefulWidget {
+  ManipulatingBall({Key key, this.onDrag});
+
+  final Function onDrag;
+
+  @override
+  _ManipulatingBallState createState() => _ManipulatingBallState();
+}
+
+class _ManipulatingBallState extends State<ManipulatingBall> {
+  double initX;
+  double initY;
+
+  _handleDrag(details) {
+    setState(() {
+      initX = details.globalPosition.dx;
+      initY = details.globalPosition.dy;
+    });
+  }
+
+  _handleUpdate(details) {
+    var dx = details.globalPosition.dx - initX;
+    var dy = details.globalPosition.dy - initY;
+    initX = details.globalPosition.dx;
+    initY = details.globalPosition.dy;
+    widget.onDrag(dx, dy);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // onVerticalDragDown: (drag) {},
+      onPanStart: _handleDrag,
+      onPanUpdate: _handleUpdate,
+      child: Container(
+        width: ballDiameter,
+        height: ballDiameter,
+        decoration: BoxDecoration(
+          color: Colors.blue.withOpacity(0.5),
+          shape: BoxShape.circle,
+        ),
+      ),
     );
   }
 }
